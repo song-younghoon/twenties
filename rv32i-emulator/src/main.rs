e4b88ac949983ca9c31acb1c1d75d63d17200623
song-younghoon <ontoro339@gmail.com>
2026-08-28 14:15:34 +0900

    implement program counter

--- rv32i-emulator/src/main.rs
+++ rv32i-emulator/src/main.rs
@@ -22,19 +22,16 @@ fn set_register(index: usize, registers: &mut [u32; 32], value: u32) {
     match index {
         0 => {}
         1..32 => registers[index] = value,
-        _ => panic!("not exists register")
+        _ => panic!("not exists register"),
     }
 }
 
 fn sign_extend_12(unsigned_12: u32) -> u32 {
-    return (((unsigned_12 << 20) as i32) >> 20) as u32
+    return (((unsigned_12 << 20) as i32) >> 20) as u32;
 }
 
 fn addi(rd: usize, rs1: usize, imm: u32, registers: &mut [u32; 32]) {
-    println!(
-        "[ADDI] rd: {:#X}, rs1: {:#X}, imm: {:#X}",
-        rd, rs1, imm
-    );
+    println!("[ADDI] rd: {:#X}, rs1: {:#X}, imm: {:#X}", rd, rs1, imm);
     let extended_imm: u32 = sign_extend_12(imm);
     set_register(rd, registers, registers[rs1].wrapping_add(extended_imm));
 }
@@ -55,22 +52,39 @@ fn execute_instruction(instruction: u32, registers: &mut [u32; 32], pc: &mut u32
     match opcode {
         0b0010011 => match funct3 {
             0b000 => addi(rd, rs1, imm, registers),
-            _ => panic!("unsupported funct3")
+            _ => panic!("unsupported funct3"),
         },
-        _ => panic!("unsupported opcode")
+        _ => panic!("unsupported opcode"),
     }
-
-    *pc += 4;
 }
 
 struct Emulator {
     registers: [u32; 32],
     pc: u32,
+    memory: [u8; 2048],
 }
 
 impl Emulator {
     fn new() -> Emulator {
-        Emulator { registers: [0; 32], pc: 0 }
+        Emulator {
+            registers: [0; 32],
+            pc: 0,
+            memory: [0; 2048],
+        }
+    }
+
+    fn fetch_instruction(&self) -> u32 {
+        let pc = self.pc as usize;
+        return self.memory[pc] as u32
+            | ((self.memory[pc + 1] as u32) << 8)
+            | ((self.memory[pc + 2] as u32) << 16)
+            | ((self.memory[pc + 3] as u32) << 24);
+    }
+
+    fn next(&mut self) {
+        let instruction = self.fetch_instruction();
+        self.execute(instruction);
+        self.pc += 4;
     }
 
     fn execute(&mut self, instruction: u32) {
@@ -80,11 +94,18 @@ impl Emulator {
 
 fn main() {
     let mut emulator: Emulator = Emulator::new();
+    // addi x1, x0, 5 = 00 50 00 93
+    emulator.memory[0..4].copy_from_slice(&[0x93, 0x00, 0x50, 0x00]);
+    // addi x2, x1, 10 = 00 a0 81 13
+    emulator.memory[4..8].copy_from_slice(&[0x13, 0x81, 0xa0, 0x00]);
+    // addi x3, x2, -3 = ff d1 01 93
+    emulator.memory[8..12].copy_from_slice(&[0x93, 0x01, 0xd1, 0xff]);
 
-    emulator.registers[12] = 10;
-    emulator.execute(0xfff60393);
+    emulator.next();
+    emulator.next();
+    emulator.next();
 
-    println!("register[7]: {}", emulator.registers[7]);
+    println!("{}", emulator.registers[3]);
 }
 
 #[cfg(test)]
@@ -145,5 +166,28 @@ mod tests {
         emulator.execute(0b1111_1111_1111_00111_000_00111_0010011);
         println!("{}", emulator.registers[7]);
         assert_eq!(emulator.registers[7], 0xFFFFFFFF);
+    }
+
+    #[test]
+    fn executes_multiple_addi_instructions() {
+        let mut emulator = Emulator::new();
+
+        // addi x1, x0, 5
+        emulator.memory[0..4].copy_from_slice(&[0x93, 0x00, 0x50, 0x00]);
+
+        // addi x2, x1, 10
+        emulator.memory[4..8].copy_from_slice(&[0x13, 0x81, 0xa0, 0x00]);
+
+        // addi x3, x2, -3
+        emulator.memory[8..12].copy_from_slice(&[0x93, 0x01, 0xd1, 0xff]);
+
+        emulator.next();
+        emulator.next();
+        emulator.next();
+
+        assert_eq!(emulator.registers[1], 5);
+        assert_eq!(emulator.registers[2], 15);
+        assert_eq!(emulator.registers[3], 12);
+        assert_eq!(emulator.pc, 12);
     }
 }
